f85cdc0dce9c1914a9807cca794cbbc921f49eab
LongMyDu <63798275+LongMyDu@users.noreply.github.com>
2024-05-08 18:18:44 +0700

    Update test_1.js

--- test_1.js
+++ test_1.js
@@ -1,14 +1,3 @@
-var $ = document;
-var popup = getPopupProvider();
-
-function toast(text) {
-    popup.showToast(text);
-}
-
-function node(id) {
-   return $.getElementById(id);
-}
-
 function func_1() {
     node("test_1").style.backgroundColor = "bisque";
     getPopupProvider().showToast("Hellooo! This is function 1 downloaded.")
